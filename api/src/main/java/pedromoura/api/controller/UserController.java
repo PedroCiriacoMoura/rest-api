fac4a62f09873d71e76ddf925a0cf91d470b64d0
PedroCiriacoMoura <113620611+PedroCiriacoMoura@users.noreply.github.com>
2022-09-15 13:27:41 -0300

    password encryption

--- api/src/main/java/pedromoura/api/controller/UserController.java
+++ api/src/main/java/pedromoura/api/controller/UserController.java
@@ -3,6 +3,7 @@ package pedromoura.api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import pedromoura.api.model.UserModel;
 import pedromoura.api.repository.UserRepository;
@@ -14,7 +15,13 @@ import java.util.List;
 public class UserController {
 
     @Autowired
-    private UserRepository repository;
+    private final UserRepository repository;
+    private final PasswordEncoder encoder;
+
+    public UserController(UserRepository repository, PasswordEncoder encoder) {
+        this.repository = repository;
+        this.encoder = encoder;
+    }
 
     @GetMapping(path = "/user/list")
     public List<UserModel> list () {
@@ -24,6 +31,7 @@ public class UserController {
     @PostMapping(path = "/user/add")
     @ResponseStatus(HttpStatus.CREATED)
     public UserModel add (@RequestBody UserModel user) {
+        user.setPassword(encoder.encode(user.getPassword()));
        return repository.save(user);
     }
 
